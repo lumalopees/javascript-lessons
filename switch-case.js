34444bdccde5e169941b140ef011e000c5163e04
Luma <luuh_lopees@live.com>
2023-08-20 14:20:12 -0300

    complete swich case

--- switch-case.js
+++ switch-case.js
@@ -25,6 +25,21 @@ switch (mesDoAno) {
   case 'Março':
     console.log('Verão');
     break;
+  case 'Abril':
+  case 'Maio':
+  case 'Junho':
+    console.log("Outono");
+    break;
+  case 'Julho':
+  case 'Agosto':
+  case 'Setembro':
+    console.log('Inverno!');
+    break;
+  case 'Outubro':
+  case 'Novembro':
+  case 'Dezembro':
+    console.log('Primavera"');
+    break;
   default: 
     console.log('Mês inválido');
 }
