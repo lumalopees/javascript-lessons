95b67e143db1f10e0217dd3bff2f9e0943aedbe2
Luma <luuh_lopees@live.com>
2023-08-20 14:14:10 -0300

    if-tenario-fim

---
+++ if-ternario.js
@@ -0,0 +1,12 @@
+const valor = 10;
+let checagem;
+
+// if (valor > 10) {
+//   checagem = true;
+// } else {
+//   checagem = false;
+// }
+
+checagem = valor > 10 ? true : false;
+
+console.log(checagem);
