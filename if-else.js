e2dccb45a2b71c1d439adc512db193830b40af7b
Luma <luuh_lopees@live.com>
2023-08-20 13:42:02 -0300

    if-else

---
+++ if-else.js
@@ -0,0 +1,17 @@
+// Estrutura Condicional (Condição)
+// IF = Se
+// ELSE = Senão
+// ELSE IF = Senão se
+// SE a primeira porta estiver aberta, você entra, SE NÃO, vai pela segunda.
+
+
+let num1 = 2;
+let num2 = 2;
+
+if (num1 > num2) { 
+  console.log('num1 é maior');
+} else if (num1 < num2) {
+  console.log('num1 é menor');
+} else {
+  console.log('num1 é igual');
+}
