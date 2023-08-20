ccf3f4c0a87abf36a3c2d940de1393d0598bc7d8
Luma <luuh_lopees@live.com>
2023-08-20 11:27:09 -0300

    add: comparison operators

--- learning.js
+++ learning.js
@@ -4,4 +4,33 @@
 let mensagem = 'hello world';
 console.log (typeof mensagem);
 
-// Tipos Primitivos 
+// Tipos Primitivos - Tipagem Dinâmica
+// String (palavras entre ''), number (números), boolean (true, false), undefined (apenas funciona em let, valores indefinido), null (valor de objeto)
+
+let xablau = null;
+console.log(xablau);
+
+// Operadores - símbolos
+//  Atribuição (=)
+// Soma (+)
+// Subtração (-)
+// Multiplicação (*)
+// Potência (**)
+// Módulo/resto (%)
+
+let num1 = 3;
+let num2 = 2;
+let soma = num1 + num2
+console.log(soma)
+
+
+// Comparação 
+// Igualdade (==)
+// Valor absolutamente igual (===)
+// Maior (>)
+// Menor (<)
+// Maior que (>=)
+// Menor que (<=)
+
+// ex: 3 == '3' (TRUE, pois os VALORES são iguais)
+// ex: 3 === '3' (FALSE, pois os VALORES são iguais, mas não os TIPOS - um NUMBER e uma STRING)
