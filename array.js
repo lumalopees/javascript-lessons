a4bff53141f79699ff22f825c5e2e0681bfef499
Luma <luuh_lopees@live.com>
2023-08-20 20:46:19 -0300

    finalizando array

--- array.js
+++ array.js
@@ -1,3 +1,15 @@
 // Variável 
 
 const nomesEstudantes36 = ['Michael', 'Nicolas', 'Amanda', 'Luma', 'Ana']
+
+console.log(nomesEstudantes36[0]);
+console.log(nomesEstudantes36[1]);
+console.log(nomesEstudantes36[2]);
+console.log(nomesEstudantes36[3]);
+console.log(nomesEstudantes36[4]);
+
+const numeros = [1, 2, 3, 4, 5, 6];
+console.log(numeros[numeros.length - 1]);
+
+const arrayAleatorio = ['Enza', 25, 9999999];
+console.log(arrayAleatorio);
