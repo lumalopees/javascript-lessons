7329948c66a9316fb63c4a9e1f49ab77892c1a90
Luma <luuh_lopees@live.com>
2023-08-20 21:19:26 -0300

    mais four

---
+++ for.js
@@ -0,0 +1,5 @@
+const nomesEstudantes36 = ['Michael', 'Nicolas', 'Amanda', 'Luma', 'Ana', 'Breno'] 
+
+for (let index = 0; index < nomesEstudantes36.length; index += 1) {
+  console.log(nomesEstudantes36[index]);
+}
