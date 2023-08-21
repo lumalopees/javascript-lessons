817ec1780aa24c250c5861d5abedb199aa718402
Luma <luuh_lopees@live.com>
2023-08-20 22:31:27 -0300

    feat: utilizando template literals

--- for.js
+++ for.js
@@ -3,3 +3,7 @@ const nomesEstudantes36 = ['Michael', 'Nicolas', 'Amanda', 'Luma', 'Ana', 'Breno
 for (let index = 0; index < nomesEstudantes36.length; index += 1) {
   console.log(nomesEstudantes36[index]);
 }
+
+for (let repeticao = 1; repeticao <= 50; repeticao +=1) {
+  console.log(`A turma ${repeticao} é demais!!`);
+}
