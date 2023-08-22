dfc75fc3567f772c3543f197b75f1f4e82965ea6
Luma <luuh_lopees@live.com>
2023-08-22 20:24:13 -0300

    add: arrow function

---
+++ arrow-function.js
@@ -0,0 +1,9 @@
+// function boasVindas (pessoaEstudante) {
+//   return `Olá, ${pessoaEstudante}! Tudo bem com você?`
+// }
+
+const boasVindas = (pessoaEstudante) => {
+  return `Olá, ${pessoaEstudante}! Tudo bem com você?`;
+}
+
+console.log(boasVindas('Joy'));
