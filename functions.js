ff0595a9b7e4b96de002f9dc7a95167edf1e9beb
Luma <luuh_lopees@live.com>
2023-08-22 20:17:06 -0300

    add: basic function

---
+++ functions.js
@@ -0,0 +1,11 @@
+// Função para boas vinda
+
+function boasVindas (pessoaEstudante) {
+  return `Olá, ${pessoaEstudante}! Tudo bem com você?`
+}
+
+console.log(boasVindas('Joy'));
+console.log(boasVindas('Clay'));
+console.log(boasVindas('Allan'));
+console.log(boasVindas('Ryan'));
+console.log(boasVindas('Esther'));
